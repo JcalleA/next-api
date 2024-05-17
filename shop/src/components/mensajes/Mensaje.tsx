39065a750396db4a0e2ecd57f6b666e94859335f
Jhonk1413 <kalle-1413@hotmail.com>
2024-05-17 11:39:24 -0500

    ok

--- shop/src/components/mensajes/Mensaje.tsx
+++ shop/src/components/mensajes/Mensaje.tsx
@@ -8,7 +8,7 @@ export const Mensaje = () => {
 
 
   return (
-    <div className=" relative w-[80%] mx-auto mt-1">
+    <div className=" relative w-[95%] mx-auto mt-1 sm:w-[80%]">
         <Image
         className=" rounded-lg"
         src={"/imgs/PROMO.png"} 
